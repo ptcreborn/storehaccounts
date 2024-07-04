0a1e15f7377c2f343a1df70fded84366519e5e20
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-07-04 19:25:19 +0800

    Update quillEditor.js

--- quillEditor.js
+++ quillEditor.js
@@ -1,3 +1,4 @@
+/*
 
 
 var quillEditor = {
@@ -21,6 +22,62 @@ var quillEditor = {
 
                     [{
                             'size': ['small', false, 'large']
+                        }
+                    ], // custom dropdown
+                    [{
+                            'align': ''
+                        }, {
+                            'align': 'center'
+                        }, {
+                            'align': 'right'
+                        }
+                    ]
+                ],
+            },
+            placeholder: "Compose a thread",
+            theme: 'snow', // or 'bubble'
+        });
+    },
+
+    setContent: function (htmlValue) {
+        quillEditor.quill.clipboard.dangerouslyPasteHTML(0, htmlValue);
+    },
+
+    getContent: function () {
+        return quillEditor.quill.getSemanticHTML();
+    },
+
+    getText: function() {
+        return quillEditor.quill.getText();
+    }
+}
+
+*/
+
+
+
+
+var quillEditor = {
+    quill: '',
+    init: function (elem_id) {
+        quillEditor.quill = new Quill('#' + elem_id, {
+            modules: {
+                toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'], // toggled buttons
+                    ['blockquote'],
+                     // custom button values , 'code-block'['link']
+
+                    [{
+                            'list': 'ordered'
+                        }, {
+                            'list': 'bullet'
+                        }, {
+                            'list': 'check'
+                        }
+                    ],
+
+                    [{
+                            'size': ['large']
                         }
                     ], // custom dropdown
                     [{
