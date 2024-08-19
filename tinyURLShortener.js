cb7d5856ad1f2c4da44159ecefb5e5eca3774ab4
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-08-19 14:37:47 +0800

    Create tinyURLShortener.js

---
+++ tinyURLShortener.js
@@ -0,0 +1,24 @@
+
+var tinyURLShortener = {
+    shortenUrl: async function (url) {
+        let new_url = '';
+        let body = {
+            url: 'https://stackoverflow.com/questions/66991259/how-to-make-a-fetch-request-to-tinyurl',
+            domain: 'tiny.one'
+        }
+
+        let response = await fetch('https://api.tinyurl.com/create', {
+            method: 'POST',
+            headers: {
+                accept: 'application/json',
+                authorization: 'Bearer 2nLQGpsuegHP8l8J0Uq1TsVkCzP3un3T23uQ5YovVf5lvvGOucGmFOYRVj6L',
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(body)
+        });
+
+        response = await response.json();
+
+        return response.data.tiny_url;
+    }
+}
