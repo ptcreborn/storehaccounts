397d7ebeb2b45b0e633d7cd5ddb243d8ce363a49
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2024-09-03 23:51:04 +0800

    Update ImgurXHR.js

--- ImgurXHR.js
+++ ImgurXHR.js
@@ -6,27 +6,30 @@
 
 var ImgurXHR = {
     uploadImgUr: function (inputID, imgLink) {
-        document.getElementById(inputID).addEventListener('change', function (e, imgLink) {
-            var file = e.target.files[0];
-            if (!file || !file.type.match(/image.*/))
-                return;
+        document.getElementById(inputID).addEventListener('change', async function (e, imgLink) {
+            await new Promise(function (resolve, reject) {
+                var file = e.target.files[0];
+                if (!file || !file.type.match(/image.*/))
+                    return;
 
-            var fd = new FormData();
-            fd.append("image", file); // Append the file
-            var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
-            xhr.open("POST", "https://api.imgur.com/3/image.json"); // Boooom!
-            xhr.onload = function () {
+                var fd = new FormData();
+                fd.append("image", file); // Append the file
+                var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+                xhr.open("POST", "https://api.imgur.com/3/image.json"); // Boooom!
+                xhr.onload = function () {
 
-                if (xhr.status == 200) {
-                    imgLink = JSON.parse(xhr.responseText).data.link;
-                    console.log('done uploading: ' + imgLink);
-                } else {
-                    window.alert('ImgurXHR error: Error in uploading... Please try again');
-                    imgLink = "error uploading";
+                    if (xhr.status == 200) {
+                        img_file = JSON.parse(xhr.responseText).data.link;
+						resolve(imgLink);
+                    } else {
+                        window.alert('ImgurXHR error: Error in uploading... Please try again');
+						reject(imgLink);
+                    }
                 }
-            }
-            xhr.setRequestHeader('Authorization', 'Client-ID FOO');
-            xhr.send(fd);
+                xhr.setRequestHeader('Authorization', 'Client-ID FOO');
+                xhr.send(fd);
+            });
         }, false);
     }
 }
+
