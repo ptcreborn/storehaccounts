784aadc337e7c8407e7e81517a633a8a7d095de4
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-01-29 21:30:26 +0800

    Update FirebaseApp.js

--- FirebaseApp.js
+++ FirebaseApp.js
@@ -1,102 +1,114 @@
- import {
-     initializeApp
- }
- from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
- import {
-     getAuth,
-     GoogleAuthProvider,
-     signInWithPopup
- }
- from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+import {
+    initializeApp
+}
+from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
- import {
-     getDatabase,
-     ref,
-     set,
-     child,
-     get
- }
- from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
- 
- export var FirebaseApp = {
-     firebaseConfig: '',
-     app: '',
-     auth: '',
-     provider: '',
+import {
+    getDatabase,
+    ref,
+    set,
+    child,
+    get
+}
+from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
 
-     initialize: function () {
-         FirebaseApp.firebaseConfig = {
-             apiKey: "AIzaSyDXd2n0yLGZS1mkFgLRctgtVZadsOK7JIs",
-             authDomain: "login-project-2efef.firebaseapp.com",
-             databaseURL: "https://login-project-2efef-default-rtdb.firebaseio.com",
-             projectId: "login-project-2efef",
-             storageBucket: "login-project-2efef.firebasestorage.app",
-             messagingSenderId: "470352098",
-             appId: "1:470352098:web:4a095dc9e58a5ee461debb"
-         };
-         FirebaseApp.app = initializeApp(FirebaseApp.firebaseConfig);
-         FirebaseApp.auth = getAuth(FirebaseApp.app);
-         FirebaseApp.auth.languageCode = 'en';
-         FirebaseApp.provider = new GoogleAuthProvider();
-     },
+export var FirebaseApp = {
+    firebaseConfig: '',
+    app: '',
+    auth: '',
+    provider: '',
 
-     signInWithGoogle: function () {
-         signInWithPopup(FirebaseApp.auth, FirebaseApp.provider)
-         .then((result) => {
-             const credential = GoogleAuthProvider.credentialFromResult(result);
-             const token = credential.accessToken;
-             const user = result.user;
+    initialize: function () {
+        FirebaseApp.firebaseConfig = {
+            apiKey: "AIzaSyDXd2n0yLGZS1mkFgLRctgtVZadsOK7JIs",
+            authDomain: "login-project-2efef.firebaseapp.com",
+            databaseURL: "https://login-project-2efef-default-rtdb.firebaseio.com",
+            projectId: "login-project-2efef",
+            storageBucket: "login-project-2efef.firebasestorage.app",
+            messagingSenderId: "470352098",
+            appId: "1:470352098:web:4a095dc9e58a5ee461debb"
+        };
+        FirebaseApp.app = initializeApp(FirebaseApp.firebaseConfig);
+        FirebaseApp.auth = getAuth(FirebaseApp.app);
+        FirebaseApp.auth.languageCode = 'en';
+        FirebaseApp.provider = new GoogleAuthProvider();
+    },
 
-             let data = {
-                 "fullname": user.displayName,
-                 "emailVerified": user.emailVerified,
-                 "email": user.email,
-                 "photoURL": user.photoURL,
-                 "UID": user.uid
-             }
+    signInWithGoogle: function () {
+        signInWithPopup(FirebaseApp.auth, FirebaseApp.provider)
+        .then((result) => {
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            const user = result.user;
 
-             FirebaseApp.writeDataJSON(user.uid, "users", data);
+            let data = {
+                "fullname": user.displayName,
+                "emailVerified": user.emailVerified,
+                "email": user.email,
+                "photoURL": user.photoURL,
+                "UID": user.uid
+            }
 
-         }).catch((error) => {
-             window.alert(error);
-         })
-     },
+            FirebaseApp.writeDataJSON(user.uid, "users", data);
 
-     verifyUserLogStatus: function () {		 
-		 console.log(FirebaseApp.auth);
-         FirebaseApp.auth.onAuthStateChanged(function (user) {			 
-             if (user)
-                 return true;
-             else
-                 return false;
-         });
-     },
+        }).catch((error) => {
+            window.error(error);
+        })
+    },
 
-     signOutUser: function (callback) {
-		 console.log(FirebaseApp.auth);
-         FirebaseApp.auth.signOut(() => {
-             window.alert('logged out!');
-             callback();
-         }, (e) => {
-             window.alert('error: ' + e);
-         });
-     },
+    isUserLoggedIn: async function () {
+        let isUserLoggedIn = false;
+        try {
+            await FirebaseApp.verifyUserLogStatus();
+            isUserLoggedIn = true;
+        } catch (e) {
+            isUserLoggedIn = false;
+        }
+        return isUserLoggedIn;
+    },
 
-     writeDataJSON: function (uid, path, data) {
-         const db = getDatabase();
-         set(ref(db, path + '/' + uid), data);
-     },
+    verifyUserLogStatus: async function () {
+        return new Promise((resolve, reject) => {
+            onAuthStateChanged(FirebaseApp.auth, function (user) {
+                if (user)
+                    resolve(true);
+                else
+                    reject("User not logged in.");
+            });
+        });
+    },
 
-     getDataJSON: function (path) {
-         const dbRef = ref(getDatabase());
-         get(child(dbRef, path)).then((snapshot) => {
-             if (snapshot.exists()) {
-                 return snapshot.val();
-             } else {
-                 return null;
-             }
-         }).catch((error) => {
-             window.alert(error);
-         });
-     }
- }
+    signOutUser: function () {
+        signOut(FirebaseApp.auth).then(() => {
+            console.log("Signed out successfully");
+        }).catch((error) => {
+            window.alert("Error " + error);
+        });
+    },
+
+    writeDataJSON: function (uid, path, data) {
+        const db = getDatabase();
+        set(ref(db, path + '/' + uid), data);
+    },
+
+    getDataJSON: function (path) {
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, path)).then((snapshot) => {
+            if (snapshot.exists()) {
+                return snapshot.val();
+            } else {
+                return null;
+            }
+        }).catch((error) => {
+            window.error(error);
+        });
+    }
+}
