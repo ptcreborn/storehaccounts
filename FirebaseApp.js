18cd3c547936dca0772cd4a3f530c223be4b403c
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-01-29 19:23:08 +0800

    Update FirebaseApp.js

--- FirebaseApp.js
+++ FirebaseApp.js
@@ -1,4 +1,24 @@
- var FirebaseApp = {
+ import {
+     initializeApp
+ }
+ from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+ import {
+     getAuth,
+     GoogleAuthProvider,
+     signInWithPopup
+ }
+ from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
+
+ import {
+     getDatabase,
+     ref,
+     set,
+     child,
+     get
+ }
+ from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
+
+var FirebaseApp = {
      firebaseConfig: '',
      app: '',
      auth: '',
