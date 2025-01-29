a248f8dd52c3d505a4f311a8c97ac2db4376b081
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-01-29 18:49:46 +0800

    Update FirebaseApp.js

--- FirebaseApp.js
+++ FirebaseApp.js
@@ -1,82 +1,100 @@
+ import {
+     initializeApp
+ }
+ from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+ import {
+     getAuth,
+     GoogleAuthProvider,
+     signInWithPopup
+ }
+ from "https://www.gstatic.com/firebasejs/11.2.0/firebase-auth.js";
 
+ import {
+     getDatabase,
+     ref,
+     set,
+     child,
+     get
+ }
+ from "https://www.gstatic.com/firebasejs/11.2.0/firebase-database.js";
 
-var FirebaseApp = {
-    firebaseConfig: '',
-    app: '',
-    auth: '',
-    provider: '',
+ var FirebaseApp = {
+     firebaseConfig: '',
+     app: '',
+     auth: '',
+     provider: '',
 
-    initialize: function () {       
-        FirebaseApp.firebaseConfig = {
-            apiKey: "AIzaSyDXd2n0yLGZS1mkFgLRctgtVZadsOK7JIs",
-            authDomain: "login-project-2efef.firebaseapp.com",
-            databaseURL: "https://login-project-2efef-default-rtdb.firebaseio.com",
-            projectId: "login-project-2efef",
-            storageBucket: "login-project-2efef.firebasestorage.app",
-            messagingSenderId: "470352098",
-            appId: "1:470352098:web:4a095dc9e58a5ee461debb"
-        };
-        FirebaseApp.app = initializeApp(FirebaseApp.firebaseConfig);
-        FirebaseApp.auth = getAuth(FirebaseApp.app);
-        FirebaseApp.auth.languageCode = 'en';
-        FirebaseApp.provider = new GoogleAuthProvider();
-    },
+     initialize: function () {
+         FirebaseApp.firebaseConfig = {
+             apiKey: "AIzaSyDXd2n0yLGZS1mkFgLRctgtVZadsOK7JIs",
+             authDomain: "login-project-2efef.firebaseapp.com",
+             databaseURL: "https://login-project-2efef-default-rtdb.firebaseio.com",
+             projectId: "login-project-2efef",
+             storageBucket: "login-project-2efef.firebasestorage.app",
+             messagingSenderId: "470352098",
+             appId: "1:470352098:web:4a095dc9e58a5ee461debb"
+         };
+         FirebaseApp.app = initializeApp(FirebaseApp.firebaseConfig);
+         FirebaseApp.auth = getAuth(FirebaseApp.app);
+         FirebaseApp.auth.languageCode = 'en';
+         FirebaseApp.provider = new GoogleAuthProvider();
+     },
 
-    signInWithGoogle: function () {
-        signInWithPopup(FirebaseApp.auth, FirebaseApp.provider)
-        .then((result) => {
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            const user = result.user;
+     signInWithGoogle: function () {
+         signInWithPopup(FirebaseApp.auth, FirebaseApp.provider)
+         .then((result) => {
+             const credential = GoogleAuthProvider.credentialFromResult(result);
+             const token = credential.accessToken;
+             const user = result.user;
 
-            let data = {
-                "fullname": user.displayName,
-                "emailVerified": user.emailVerified,
-                "email": user.email,
-                "photoURL": user.photoURL,
-                "UID": user.uid
-            }
+             let data = {
+                 "fullname": user.displayName,
+                 "emailVerified": user.emailVerified,
+                 "email": user.email,
+                 "photoURL": user.photoURL,
+                 "UID": user.uid
+             }
 
-            FirebaseApp.writeDataJSON(user.uid, "users", data);
+             FirebaseApp.writeDataJSON(user.uid, "users", data);
 
-        }).catch((error) => {
-            window.error(error);
-        })
-    },
+         }).catch((error) => {
+             window.error(error);
+         })
+     },
 
-    verifyUserLogStatus: function () {
-        FirebaseApp.auth.onAuthStateChanged(function (user) {
-            if (user)
-                return true;
-            else
-                return false;
-        });
-    },
+     verifyUserLogStatus: function () {
+         FirebaseApp.auth.onAuthStateChanged(function (user) {
+             if (user)
+                 return true;
+             else
+                 return false;
+         });
+     },
 
-    signOutUser: function (callback) {
-        FirebaseApp.auth.signOut(() => {
-            window.alert('logged out!');
-            callback();
-        }, (e) => {
-            window.error('error: ' + e);
-        });
-    },
+     signOutUser: function (callback) {
+         FirebaseApp.auth.signOut(() => {
+             window.alert('logged out!');
+             callback();
+         }, (e) => {
+             window.error('error: ' + e);
+         });
+     },
 
-    writeDataJSON: function (uid, path, data) {
-        const db = getDatabase();
-        set(ref(db, path + '/' + uid), data);
-    },
+     writeDataJSON: function (uid, path, data) {
+         const db = getDatabase();
+         set(ref(db, path + '/' + uid), data);
+     },
 
-    getDataJSON: function (path) {
-        const dbRef = ref(getDatabase());
-        get(child(dbRef, path)).then((snapshot) => {
-            if (snapshot.exists()) {
-                return snapshot.val();
-            } else {
-                return null;
-            }
-        }).catch((error) => {
-            window.error(error);
-        });
-    }
-}
+     getDataJSON: function (path) {
+         const dbRef = ref(getDatabase());
+         get(child(dbRef, path)).then((snapshot) => {
+             if (snapshot.exists()) {
+                 return snapshot.val();
+             } else {
+                 return null;
+             }
+         }).catch((error) => {
+             window.error(error);
+         });
+     }
+ }
