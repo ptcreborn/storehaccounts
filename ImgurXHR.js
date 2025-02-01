e2493f520050917448b421f4067beca56d583e36
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-01 14:07:33 +0800

    Create ImgurXHR.js

---
+++ ImgurXHR.js
@@ -0,0 +1,42 @@
+// USAGE YOU MUST HAVE INPUT ELEMENT
+// ADD THE INPUT ID IN THE FUNCTION PARAMETER
+// ADD THE OBJECT VARIABLE IN THE 2ND PARAMETER
+// THE OBJECT IMGLINK WILL HOLD THE LINK OF THE IMAGE BEING UPLOADED.
+
+// imgDIV is the div that holds all the uploaded images.
+
+
+var ImgurXHR = {
+    uploadImgUr: function (inputID, imgDIV, uploadCallback, doneUploadCallback) {
+        document.getElementById(inputID).addEventListener('change', function (e) {
+            if(!uploadCallback()) uploadCallback();
+            var file = e.target.files[0];
+            if (!file || !file.type.match(/image.*/))
+                return;
+
+            var fd = new FormData();
+            fd.append("image", file); // Append the file
+            var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+            xhr.open("POST", "https://api.imgur.com/3/image"); // Boooom!
+            xhr.onload = function () {
+
+                if (xhr.status == 200) {
+                    let temp_img = document.createElement('img');
+                    temp_img.src = JSON.parse(xhr.responseText).data.link;
+                    imgDIV.innerHMTL = '';
+                    imgDIV.appendChild(temp_img);
+                    if(!doneUploadCallback()) doneUploadCallback();
+                    // callback(img_file);
+                } else {
+                    window.alert('ImgurXHR error: Error in uploading... Please try again');
+                    imgLink.error = "Error Uploading in ImgUr";
+                    if(!doneUploadCallback()) doneUploadCallback();
+                }
+            }
+            xhr.setRequestHeader('Authorization', 'Client-ID 33f63d5902f27e5');
+            xhr.send(fd);
+        }, false);
+    }
+}
+
+
