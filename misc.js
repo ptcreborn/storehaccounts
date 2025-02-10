aa6103d2e551a36076de4d75fb694e4309f5450b
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-10 23:39:22 +0800

    Update misc.js

--- misc.js
+++ misc.js
@@ -1,17 +1,19 @@
 
 
-export function query(id) {
+function query(id) {
   return document.getElementById(id);
 }
 
-export function disableElem(elem) {
+function disableElem(elem) {
     elem.style.opacity = '0.3';
     elem.style.pointerEvents = 'none';
     elem.disabled = true;
 }
 
-export function enableElem(elem) {
+function enableElem(elem) {
     elem.style.opacity = '1';
     elem.style.pointerEvents = 'auto';
     elem.disabled = false;
 }
+
+export {query, disableElem, enableElem};
