ceea304657cc127d076d056de23f85531c32f66c
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-12 22:20:39 +0800

    Create miscJS.js

---
+++ miscJS.js
@@ -0,0 +1,17 @@
+function query(id) {
+  return document.getElementById(id);
+}
+
+function disableElem(elemID, text) {
+    let elem = query(elemID).style;
+    elem.opacity = '0.7';
+    elem.pointerEvents = 'none';
+    elem.innerText = text;
+}
+
+function enableElem(elemID, text) {
+    let elem = query(elemID).style;
+    elem.opacity = '1';
+    elem.pointerEvents = 'auto';
+    elem.innerText = text;
+}
