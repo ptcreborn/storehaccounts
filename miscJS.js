85002a6b84e08970370afbf628409eda75348744
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-12 22:29:33 +0800

    Update miscJS.js

--- miscJS.js
+++ miscJS.js
@@ -3,15 +3,15 @@ function query(id) {
 }
 
 function disableElem(elemID, text) {
-    let elem = document.getElementById(elemID).style;
-    elem.opacity = '0.7';
-    elem.pointerEvents = 'none';
-    elem.innerText = text;
+    let elem = document.getElementById(elemID);
+    elem.style.opacity = '0.7';
+    elem.style.pointerEvents = 'none';
+    elem.style.innerText = text;
 }
 
 function enableElem(elemID, text) {
     let elem = document.getElementById(elemID).style;
-    elem.opacity = '1';
-    elem.pointerEvents = 'auto';
-    elem.innerText = text;
+    elem.style.opacity = '1';
+    elem.style.pointerEvents = 'auto';
+    elem.style.innerText = text;
 }
