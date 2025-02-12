0d160f0ff8098473478c505521a017ddeb9d6c90
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-12 22:54:28 +0800

    Update miscJS.js

--- miscJS.js
+++ miscJS.js
@@ -4,14 +4,14 @@ function query(id) {
 
 function disableElem(elemID, text) {
     let elem = query(elemID);
-    elem.opacity = '0.7';
-    elem.pointerEvents = 'none';
-    elem.innerText = text;
+    elem.style.opacity = '0.7';
+    elem.style.pointerEvents = 'none';
+    elem.style.innerText = text;
 }
 
 function enableElem(elemID, text) {
-    let elem = query(elemID).style;
-    elem.opacity = '1';
-    elem.pointerEvents = 'auto';
-    elem.innerText = text;
+    let elem = query(elemID);
+    elem.style.opacity = '1';
+    elem.style.pointerEvents = 'auto';
+    elem.style.innerText = text;
 }
