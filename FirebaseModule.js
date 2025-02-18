43041e010cf8b8435044b3aa1acff4ef2efaa00f
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-18 20:08:39 +0800

    Update FirebaseModule.js

--- FirebaseModule.js
+++ FirebaseModule.js
@@ -43,5 +43,27 @@ var FirebaseModule = {
             req.setRequestHeader('Content-Type', 'application/json');
             req.send(data);
         });
+    },
+	
+	post: function (url, data) {
+        return new Promise(function (resolve, reject) {
+            let req = new XMLHttpRequest();
+
+            req.onload = () => {
+                if (req.readyState == 4)
+                    if (req.status == 200)
+                        resolve(req.response);
+                    else
+                        reject(req.response);
+            }
+
+            req.onerror = (err) => {
+                window.alert("Error detected in FirebaseModule: " + req.statusText);
+            }
+
+            req.open('POST', url, true);
+            req.setRequestHeader('Content-Type', 'application/json');
+            req.send(data);
+        });
     }
 }
