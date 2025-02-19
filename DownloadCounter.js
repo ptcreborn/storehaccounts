c6d3308a5e9865c92f589778ff9428b841584dcc
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-19 13:07:05 +0800

    Update DownloadCounter.js

--- DownloadCounter.js
+++ DownloadCounter.js
@@ -1,12 +1,13 @@
-'(async function () {window.addEventListener(\"load\"),async function () {query(" + request_url + ").querySelector(\"b\")[1].innerText = await FirebaseModule.get(\"https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/" + request_url + "/clicks.json\");query(" + request_url + ").querySelector(\"b\")[2].innerText = await FirebaseModule.get(\"https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/" + request_url + "/downloads.json\");},false);let currentCount = await FirebaseModule.get(\"https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/" + request_url + "/clicks.json\");await FirebaseModule.patch(\"https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/" + request_url + ".json\", JSON.stringify({\"clicks\": ++currentCount}));query(" + request_url + ").querySelector(\"b\")[1].innerText = currentCount;window.location.href = \"https://storehaccounts.blogspot.com/p/link-terminal.html#fb?" + request_url + ";\"})();'
+
 
 window.addEventListener('load', async function () {
     let allDownloadsBtns = document.querySelectorAll('button');
     allDownloadsBtns = Array.from(allDownloadsBtns);
 
-    allDownloadsBtns.filter(items => {return items.id.charAt(0) === '-' ? items.id : null});
+    allDownloadsBtns = allDownloadsBtns.filter(items => {return items.id.charAt(0) === '-' ? items : null});
 
-    allDownloadsBtns.forEach(id => {
+    allDownloadsBtns.forEach(elems => {
+		let id = elems.id;
         get_dl_views(id);
         get_dl_counts(id);
         query(id).onclick = async() => {
@@ -20,7 +21,7 @@ window.addEventListener('load', async function () {
     });
 }, false);
 
-function get_dl_views(url) {
+async function get_dl_views(url) {
     let LT_url = 'https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/';
     let data = await FirebaseModule.get(LT_url + url + ".json");
     data = JSON.parse(data).clicks;
@@ -28,7 +29,7 @@ function get_dl_views(url) {
     query(url).querySelector('b')[1].innerText = data;
 }
 
-function get_dl_counts() {
+async function get_dl_counts(url) {
     let LT_url = 'https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/';
     let data = await FirebaseModule.get(LT_url + url + ".json");
     data = JSON.parse(data).downloads;
