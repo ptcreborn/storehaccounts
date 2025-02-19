8e9b6ff3afd5b147c5f7b5d641bea0e90a6e9d32
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-19 12:27:15 +0800

    Create DownloadCounter.js

---
+++ DownloadCounter.js
@@ -0,0 +1,39 @@
+window.addEventListener('load', async function () {
+    let allDownloadsBtns = document.querySelectorAll('button');
+  	console.log(allDownloadsBtns);
+  
+    allDownloadsBtns = Array.from(allDownloadsBtns);
+
+    allDownloadsBtns = allDownloadsBtns.filter(items => {
+      	return items.id.charAt(0) === '-' ? items : null;
+    });
+  
+    allDownloadsBtns.forEach(elem => {
+      	let id = elem.id;
+        get_dl_views(id);
+        get_dl_counts(id);
+        query(id).onclick = async() => {
+            let curCount = await FirebaseModule.get('https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/' + id + '.json');
+            await FirebaseModule.patch('https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/' + id + '.json', JSON.stringify({
+                    "clicks": ++curCount
+                }));
+            window.location.href = "https://storehaccounts.blogspot.com/p/link-terminal.html#fb?" + id;
+        }
+    });
+}, false);
+
+async function get_dl_views(url) {
+    let LT_url = 'https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/';
+    let data = await FirebaseModule.get(LT_url + url + ".json");
+    data = JSON.parse(data).clicks;
+
+    query(url).querySelectorAll('b')[1].innerText = data;
+}
+
+async function get_dl_counts(url) {
+    let LT_url = 'https://storehaccounts-users-default-rtdb.firebaseio.com/link_terminal/';
+    let data = await FirebaseModule.get(LT_url + url + ".json");
+    data = JSON.parse(data).downloads;
+
+    query(url).querySelectorAll('b')[2].innerText = data;
+}
