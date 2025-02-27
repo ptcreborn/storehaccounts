bf1962b94ec1665d646168c01001342ab3b8e87a
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-02-27 16:09:12 +0800

    Update storehaccounts-func-3.js

--- storehaccounts-func-3.js
+++ storehaccounts-func-3.js
@@ -70,6 +70,7 @@ function letsPreview(myData) {
 }
 
 (function () {
+	initFunctions(['ZerNotifications']);
     let shareThis = document.createElement('script');
     shareThis.setAttribute('async', 'async');
     shareThis.setAttribute('src', 'https://platform-api.sharethis.com/js/sharethis.js#property=6501dd359dda6c0012ae1168&product=inline-reaction-buttons&source=platform');
