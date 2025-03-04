d526726739b60f0ff03403a9dc230a3a187eb170
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-03-04 13:51:00 +0800

    Update DownloadCounter1.js

--- DownloadCounter1.js
+++ DownloadCounter1.js
@@ -1,30 +1,33 @@
 (async() => {
-	await initFunctions(['FirebaseModule']);
-	let allBtns = document.querySelectorAll('button[disabled]');
-	let filterBtn = Array.from(allBtns);
-	filterBtn = filterBtn.filter(items => {
-		return items.id.substring(0, 4) === "ndm-" ? items : null;
-	});
-	
-	for(let i=0; i<filterBtn.length; i++) {
-		let spanCtr = filterBtn[i].querySelector('span');
-		let btn = filterBtn[i];
-		let key = '-' + filterBtn[i].id.split('ndm-')[1];
-		let db = 'https://account-requests-default-rtdb.firebaseio.com/shortenedLinks';
-		
-		let data = await FirebaseModule.get(db + '/' + key + '.json');
-		data = JSON.parse(data);
-		spanCtr.innerText = data.count;
-		
-		btn.onclick = async() => {
-			await FirebaseModule.patch(db + '/' + key + '.json', JSON.stringify({
-				'count': parseInt(data.count) + 1
-			}));
-			spanCtr.innerText = data.count + 1;
-			btn.style.pointerEvents = 'none';
-			btn.style.opacity = '0.8';
-			window.location.href = 'https://storehaccounts.blogspot.com/p/link-terminal.html?' + key;
-		}
-		btn.removeAttribute('disabled');
-	}
+    await initFunctions(['FirebaseModule']);
+    let allBtns = document.querySelectorAll('button[disabled]');
+    let filterBtn = Array.from(allBtns);
+    filterBtn = filterBtn.filter(items => {
+        return items.id.substring(0, 4) === "ndm-" ? items : null;
+    });
+
+    for (let i = 0; i < filterBtn.length; i++) {
+        let spanCtr = filterBtn[i].querySelector('span');
+        let btn = filterBtn[i];
+        let key = '-' + filterBtn[i].id.split('ndm-')[1];
+        let db = 'https://account-requests-default-rtdb.firebaseio.com/shortenedLinks';
+
+        let data = await FirebaseModule.get(db + '/' + key + '.json');
+        data = JSON.parse(data);
+        spanCtr.innerText = data.count;
+
+        btn.onclick = async() => {
+            data = await FirebaseModule.get(db + '/' + key + '.json');
+            data = JSON.parse(data);
+            spanCtr.innerText = data.count;
+            await FirebaseModule.patch(db + '/' + key + '.json', JSON.stringify({
+                    'count': parseInt(data.count) + 1
+                }));
+            spanCtr.innerText = data.count + 1;
+            btn.style.pointerEvents = 'none';
+            btn.style.opacity = '0.8';
+            window.location.href = 'https://storehaccounts.blogspot.com/p/link-terminal.html?' + key;
+        }
+        btn.removeAttribute('disabled');
+    }
 })();
