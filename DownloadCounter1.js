83b3606155e67844185bac9ed061166108d40ce6
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-03-04 19:49:19 +0800

    Update DownloadCounter1.js

--- DownloadCounter1.js
+++ DownloadCounter1.js
@@ -7,23 +7,28 @@
     });
 
     for (let i = 0; i < filterBtn.length; i++) {
-        let spanCtr = filterBtn[i].querySelector('span');
+        let smallCtr = filterBtn[i].querySelector('small');
+        let spanText = filterBtn[i].querySelector('span');
+        let icon = filterBtn[i].querySelector('i');
         let btn = filterBtn[i];
         let key = '-' + filterBtn[i].id.split('ndm-')[1];
         let db = 'https://account-requests-default-rtdb.firebaseio.com/shortenedLinks';
+		
+		spanText.innerText = 'DOWNLOAD';
+		icon.className = 'cloud download icon';
 
         let data = await FirebaseModule.get(db + '/' + key + '.json');
         data = JSON.parse(data);
-        spanCtr.innerText = data.count;
+        smallCtr.innerText = data.count;
 
         btn.onclick = async() => {
             data = await FirebaseModule.get(db + '/' + key + '.json');
             data = JSON.parse(data);
-            spanCtr.innerText = data.count;
+            smallCtr.innerText = data.count;
             await FirebaseModule.patch(db + '/' + key + '.json', JSON.stringify({
                     'count': parseInt(data.count) + 1
                 }));
-            spanCtr.innerText = data.count + 1;
+            smallCtr.innerText = data.count + 1;
             btn.style.pointerEvents = 'none';
             btn.style.opacity = '0.8';
             window.location.href = 'https://storehaccounts.blogspot.com/p/link-terminal.html?' + key;
