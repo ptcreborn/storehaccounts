d53b3190cebb1778c7d7ff412a54115d01c02c50
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-03-08 15:56:36 +0800

    Update DirectDownloader.js

--- DirectDownloader.js
+++ DirectDownloader.js
@@ -1,38 +1,52 @@
-window.addEventListener('load', async() => {
-    document.querySelector('#generate').onclick = async() => {
-        console.log('clicked!');
-        let db = 'https://account-requests-default-rtdb.firebaseio.com/shortenedLinks.json';
-        let link = document.querySelector('#link').value;
-        try {
-            if (!new URL(link).hostname.includes('mediafire.com'))
-                throw new Error('Not Mediafire!');
-        } catch (e) {
-            window.alert('Only Mediafire Link acceptable! ' + e);
-        }
-        let numads = document.querySelector('#numads').value;
-        let size = document.querySelector('#size').value;
-        let version = document.querySelector('#version').value;
-        let title = document.querySelector('#title').value;
-        let progress = numads;
-        link = await FirebaseModule.post(db, JSON.stringify({
-                    'count': 0,
-                    'targ': btoa(link),
-                    'numads': numads,
-                    'title': title,
-                    'version': version,
-                    'size': size + ' MB'
-                }));
-        link = JSON.parse(link).name;
-        let output = document.querySelector('#outputhtml');
-        let outputta = document.querySelector('#output');
+(async function () {
+    // this function is for direct link downloads
+    await initFunctions(['FirebaseModule', 'PTC_Cookies']);
+    let allBtns = document.querySelectorAll('button[disabled]');
+    let myBtns = Array.from(allBtns);
 
-        if (numads == 0) { // means direct link
-            output.innerHTML = '<button disabled id="dl?' + link + '" class="ui big labeled icon teal button"><i class="loading spinner icon"></i><span>FETCHING ... </span><small style="float: right;">0</small></button>';
-            outputta.value = '<button disabled id="dl?' + link + '" class="ui big labeled icon teal button"><i class="loading spinner icon"></i><span>FETCHING ... </span><small style="float: right;">0</small></button>';
-        } else {
-            output.innerHTML = '<button disabled id="ndm' + link + '" class="ui big labeled icon teal button"><i class="loading spinner icon"></i><span>FETCHING ... </span><small style="float: right;">0</small></button>';
-            outputta.value = '<button disabled id="ndm' + link + '" class="ui big labeled icon teal button"><i class="loading spinner icon"></i><span>FETCHING ... </span><small style="float: right;">0</small></button>';
-        }
+    myBtns = myBtns.filter(items => {
+        return (items.id.substring(0, 4) === 'dl?-' ? items : null);
+    });
+
+    for (let i = 0; i < myBtns.length; i++) {
+        let btn = myBtns[i];
+        let btnText = btn.querySelector('span');
+        let smallText = btn.querySelector('small');
+        let icon = filterBtn[i].querySelector('i');
+
+        let db = 'https://account-requests-default-rtdb.firebaseio.com/shortenedLinks';
+        let key = '-' + btn.id.split('dl?-')[1];
+
+        let jdata = await FirebaseModule.get(db + '/' + key + '.json');
+        jdata = JSON.parse(jdata);
+
+        icon.className = 'cloud download icon';
+        btnText.innerText = 'DOWNLOAD';
+        smallText.innerText = jdata.count;
+
+        btn.addEventListener('click', async() => {
+            if (PTC_Cookies.checkIfCookiesSupported()) {
+                let uid = '?dl=' + jdata.title + '&token='(new Date().getTime() * 17);
+                PTC_Cookies.setLocalStorage(uid, JSON.stringify({
+                        'dltime': new Date().getTime(),
+                        'expiration': 720000,
+                        'targ': jdata.targ
+
+                    }));
+
+                jdata = await FirebaseModule.get(db + '/' + key + '.json');
+                jdata = JSON.parse(jdata);
+                let curcount = jdata.count;
+
+                await FirebaseModule.patch(db + '/' + key + '.json', JSON.stringify({
+                        'count': curcount + 1
+                    }));
+
+                window.location.href = 'https://storehaccounts.blogspot.com/p/mediafire-link-generator.html?' + uid;
+            } else {
+                window.alert("Dear user, your browser does not support Cookies! Please enable them or use other browser. Thank you!");
+            }
+        }, false);
+        btn.removeAttribute('disabled');
     }
-
-}, false);
+})();
