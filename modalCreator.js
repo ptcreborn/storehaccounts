c8157142b103840c35bd68ca6fa221bf0b96e542
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-03-13 22:17:51 +0800

    Create modalCreator.js

---
+++ modalCreator.js
@@ -0,0 +1,19 @@
+function modalCreator(titleText, bodyText, buttonIcon, buttonText, moreButtonHTML) {	
+	let body = '<!--The Modal--> <div class="modal" id="loginModal"> <!--Modal content--> <div class="ui info message modal-content"> <div class="header">' + titleText + '</div><div class="content"> <small>' + bodyText + '</small><br><br> <a href="https://storehaccounts.blogspot.com/p/create-account.html" class="ui blue image label"><i class="' + buttonIcon + '"></i>' + buttonText + '</a>' + moreButtonHTML + '</div> </div> </div>';
+    let doc = document.createElement('div');
+    doc.innerHTML = body;
+    document.querySelector('body').appendChild(doc);
+    let modal = document.getElementById("loginModal");
+    let content = modal.querySelector(".modal-content");
+    content.style.width = '30%';
+
+    let btn = document.getElementById("profile-circle-header");
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
