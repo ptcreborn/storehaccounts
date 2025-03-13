851b6e64a84502d0afca5aabb7e0e60747ed05b2
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-03-13 22:24:24 +0800

    Update modalCreator.js

--- modalCreator.js
+++ modalCreator.js
@@ -1,5 +1,5 @@
-function modalCreator(modalID, titleText, bodyText, buttonIcon, buttonText, moreButtonHTML) {	
-	let body = '<!--The Modal--> <div class="modal" id="' + modalID + '"> <!--Modal content--> <div class="ui info message modal-content"> <div class="header">' + titleText + '</div><div class="content"> <small>' + bodyText + '</small><br><br> <a href="https://storehaccounts.blogspot.com/p/create-account.html" class="ui blue image label"><i class="' + buttonIcon + '"></i>' + buttonText + '</a>' + moreButtonHTML + '</div> </div> </div>';
+function modalCreator(initiatorID, modalID, titleText, bodyText, buttonIcon, buttonText, moreButtonHTML) {
+    let body = '<!--The Modal--> <div class="modal" id="' + modalID + '"> <!--Modal content--> <div class="ui info message modal-content"> <div class="header">' + titleText + '</div><div class="content"> <small>' + bodyText + '</small><br><br> <a href="https://storehaccounts.blogspot.com/p/create-account.html" class="ui blue image label"><i class="' + buttonIcon + '"></i>' + buttonText + '</a>' + moreButtonHTML + '</div> </div> </div>';
     let doc = document.createElement('div');
     doc.innerHTML = body;
     document.querySelector('body').appendChild(doc);
@@ -7,7 +7,7 @@ function modalCreator(modalID, titleText, bodyText, buttonIcon, buttonText, more
     let content = modal.querySelector(".modal-content");
     content.style.width = '30%';
 
-    let btn = document.getElementById("profile-circle-header");
+    let btn = document.getElementById(initiatorID);
     btn.onclick = function () {
         modal.style.display = "block";
     }
