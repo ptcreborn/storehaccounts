aeede8ca0b45f75b00ff0496d01d7aae07a6ef7c
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-05-23 17:42:50 +0800

    Update DownloadCounter1.js

--- DownloadCounter1.js
+++ DownloadCounter1.js
@@ -1,8 +1,8 @@
 
 
 // This function serves all Download buttons
-// that has been registered to Firebase 
-// and Mediafire Links 
+// that has been registered to Firebase
+// and Mediafire Links
 // Checking the number of ads to be bypassed
 
 
@@ -21,7 +21,7 @@
         let btn = filterBtn[i];
         let key = '-' + filterBtn[i].id.split('ndm-')[1];
         let db = 'https://account-requests-default-rtdb.firebaseio.com/shortenedLinks';
-        let uid = '';		
+        let uid = '';
 
         spanText.innerText = 'DOWNLOAD';
         icon.className = 'cloud download icon';
@@ -29,13 +29,14 @@
         let data = await FirebaseModule.get(db + '/' + key + '.json');
         data = JSON.parse(data);
         smallCtr.innerText = data.count;
-		
-		let title = data.title;
-		let version = data.version;
-		let numads = data.numads;
+
+        let title = data.title;
+        let version = data.version;
+        let numads = data.numads;
 
         btn.onclick = async() => {
-			if(numads == 0) window.location.href = 'https://storehaccounts.blogspot.com/p/mediafire-link-generator.html?download=' + title + '&id=' + btoa(key) + '=&ver=' + version;
+            if (numads == 0)
+                window.location.href = 'https://storehaccounts.blogspot.com/p/mediafire-link-generator.html?download=' + title + '&id=' + btoa(key) + '=&ver=' + version;
             else if (PTC_Cookies.checkIfStorageSupported) {
                 data = await FirebaseModule.get(db + '/' + key + '.json');
                 data = JSON.parse(data);
