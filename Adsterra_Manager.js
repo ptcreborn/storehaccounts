f55268425e6477043d111da92eaa989a86987a51
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-05-28 01:20:13 +0800

    Update Adsterra_Manager.js

--- Adsterra_Manager.js
+++ Adsterra_Manager.js
@@ -17,8 +17,8 @@
         // ADS#2
         [
             "adsterra_mid",
-            "atOptions = { 'key' : 'dbb081bd229885ddc0aa9d909e761042', 'format' : 'iframe', 'height' : 90, 'width' : 720, 'params' : {} };",
-            "//www.highperformanceformat.com/dbb081bd229885ddc0aa9d909e761042/invoke.js"
+            "atOptions = { 'key' : 'cc3b17b3d88c95500f8a72553d34420b', 'format' : 'iframe', 'height' : 90, 'width' : 720, 'params' : {} };",
+            "//www.highperformanceformat.com/cc3b17b3d88c95500f8a72553d34420b/invoke.js"
         ]
     ];
 
