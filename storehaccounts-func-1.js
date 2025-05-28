c45224c75e3b72c204619091ec6e6df701d6ad08
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-05-28 13:28:28 +0800

    Update storehaccounts-func-1.js

--- storehaccounts-func-1.js
+++ storehaccounts-func-1.js
@@ -144,6 +144,10 @@ function getCookieName(e) {
 
             document.querySelectorAll(".popup_greet .close")[0].style.opacity = '1';
             document.querySelectorAll(".popup_greet .close")[0].innerText = "X";
+			
+			
+            document.querySelectorAll(".popup_greet .close")[0].style.pointerEvents = "auto";
+            document.querySelectorAll(".popup_greet .close")[0].setAttribute("onclick", "closeoverlay_greet()");
         } else {
             document.getElementsByClassName("overlay_greet")[0].style.display = "block";
             document.getElementsByClassName("overlay_greet")[0].style.opacity = '0.0001';
