5b2a23ba382286777344400f3b5bda06dba970f3
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-10 01:43:56 +0800

    Create home_login.js

---
+++ home_login.js
@@ -0,0 +1,28 @@
+
+
+(async () => {
+    // this function is checking whether the user is logged in or not.
+
+    let user = await supabase.auth.getSession();
+
+    if (user.error) {
+        let msg = JSON.stringify(user.error);
+        window.alert("Error message : " + msg);
+
+        await initFunctions(['ModalCreator']);
+        ModalCreator.initiator(
+            'profile-circle-header',
+            'loginModal',
+            'Welcome to Community!',
+            '<br/>Sign in and create your account with Storehaccounts. <br/>Store all the progress of downloads, battle cats packs, bcu tools and account requests. <br/>Also share your work with others who can love it and we will make a better mod out of community ideas.',
+            'user circle icon',
+            'Sign in Now',
+            'https://storehaccounts.blogspot.com/p/sign-in-with-storehaccounts.html',
+            '');
+        return;
+    }
+
+    let user_data = user.data.session.user.user_metadata;
+    document.querySelector('#profile-circle-header').innerHTML = "<img src=\"" + data.avatar_url + "\"/>";
+    document.querySelector('#profile-circle-header').href = "https://storehaccounts.blogspot.com/p/profile-page.html";
+})();
