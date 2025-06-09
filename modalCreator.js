48866e68092c36b656fb12ad49db15c703f170b7
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-10 00:48:50 +0800

    Update modalCreator.js

--- modalCreator.js
+++ modalCreator.js
@@ -1,20 +1,44 @@
-function modalCreator(initiatorID, modalID, titleText, bodyText, buttonIcon, buttonText, buttonLink, moreButtonHTML) {
-    let body = '<!--The Modal--> <div class="modal" id="' + modalID + '"> <!--Modal content--> <div class="ui info message modal-content"> <div class="header">' + titleText + '</div><div class="content"> <small>' + bodyText + '</small><br><br> <a href="' + buttonLink + '" class="ui blue image label"><i class="' + buttonIcon + '"></i>' + buttonText + '</a>' + moreButtonHTML + '</div> </div> </div>';
-    let doc = document.createElement('div');
-    doc.innerHTML = body;
-    document.querySelector('body').appendChild(doc);
-    let modal = document.getElementById(modalID);
-    let content = modal.querySelector(".modal-content");
-    content.style.width = '30%';
 
-    let btn = document.getElementById(initiatorID);
-    btn.parentNode.style.display = 'block';
-    btn.onclick = function () {
+
+var Modal = {
+    initiator: function (initiatorID, modalID, titleText, bodyText, buttonIcon, buttonText, buttonLink, moreButtonHTML) {
+        let doc = document.createElement('div');
+        let body = '<!--The Modal--> <div class="modal" id="' + modalID + '"> <!--Modal content--> <div class="ui info message modal-content"> <div class="header">' + titleText + '</div><div class="content"> <small>' + bodyText + '</small><br><br> <a href="' + buttonLink + '" class="ui blue image label"><i class="' + buttonIcon + '"></i>' + buttonText + '</a>' + moreButtonHTML + '</div> </div> </div>';    
+        doc.innerHTML = body;
+        document.querySelector('body').appendChild(doc);
+        let modal = document.getElementById(modalID);
+        let content = modal.querySelector(".modal-content");
+        content.style.width = '30%';
+
+        let btn = document.getElementById(initiatorID);
+        btn.parentNode.style.display = 'block';
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                modal.remove();
+            }
+        }
+    },
+    pop: function(modalID, titleText, bodyText, buttonIcon, buttonText, buttonLink, moreButtonHTML) {
+        let doc = document.createElement('div');
+        let body = '<!--The Modal--> <div class="modal" id="' + modalID + '"> <!--Modal content--> <div class="ui info message modal-content"> <div class="header">' + titleText + '</div><div class="content"> <small>' + bodyText + '</small><br><br> <a href="' + buttonLink + '" class="ui blue image label"><i class="' + buttonIcon + '"></i>' + buttonText + '</a>' + moreButtonHTML + '</div> </div> </div>';    
+        doc.innerHTML = body;
+        document.querySelector('body').appendChild(doc);
+        let modal = document.getElementById(modalID);
+        let content = modal.querySelector(".modal-content");
+        content.style.width = '30%';
+
+        let btn = document.getElementById(initiatorID);
+        btn.parentNode.style.display = 'block';
         modal.style.display = "block";
-    }
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                modal.remove();
+            }
         }
     }
 }
