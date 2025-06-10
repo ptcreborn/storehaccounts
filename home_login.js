1df167d6cb215e473e2f85aa1c1c6212f3ed7ca6
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-11 02:32:27 +0800

    Update home_login.js

--- home_login.js
+++ home_login.js
@@ -20,23 +20,11 @@
     }
 
     if (user && user.data) {
-        let data;
-        let profile = '';
-        if (localStorage.getItem('ptc_user'))
-            if (JSON.parse(localStorage.getItem('ptc_user')).prof_img)
-                if (JSON.parse(localStorage.getItem('ptc_user')).prof_img.length > 0)
-                    profile = JSON.parse(localStorage.getItem('ptc_user')).prof_img;
-
-        if (!profile) {
-            data = await supabase.from('users').select('prof_img').eq('email', user.data.session.user.email);
-            if (data.error) {
-                window.alert("Error in retrieving profile image: " + JSON.stringify(data.error));
-                return;
-            }
-            profile = data.data[0].prof_img;
-            localStorage.setItem('ptc_user', JSON.stringify({
-                prof_img: profile
-            }));
+        let data = await supabase.from('users').select('prof_img').eq('email', user.data.session.user.email);
+        let profile = data.data[0].prof_img;
+        if (data.error) {
+            window.alert("Error in retrieving profile image: " + JSON.stringify(data.error));
+            return;
         }
         document.querySelector('#profile-circle-header').parentNode.style.display = 'block';
         document.querySelector('#profile-circle-header').innerHTML = "<img src=\"" + profile + "\"/>";
