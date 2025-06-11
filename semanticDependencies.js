1eb50697c499f4df2aaca5a975c66deaca0a181d
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-11 23:32:30 +0800

    Create semanticDependencies.js

---
+++ semanticDependencies.js
@@ -0,0 +1,13 @@
+(async () => {
+  let script = document.createElement('script');
+  let body = document.querySelector('body');
+
+  script.setAttribute('src', 'https://code.jquery.com/jquery-3.1.1.min.js');
+  script.setAttribute('integrity', 'sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=');
+  script.setAttribute('crossorigin', 'anonymous');
+
+  body.appendChild(script);
+  script = document.createElement('script');
+  script.setAttribute('src', 'https://cdn.jsdelivr.net/npm/semantic-ui@2.5.0/dist/semantic.min.js');
+  body.appendChild(script);
+})();
