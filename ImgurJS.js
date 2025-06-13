0b5a9dc957055740056dbd6c9b87dc97f99ba9a3
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-13 22:43:42 +0800

    Update ImgurJS.js

--- ImgurJS.js
+++ ImgurJS.js
@@ -22,6 +22,32 @@ var ImgurJS = {
             xhr.setRequestHeader('Authorization', 'Client-ID 33f63d5902f27e5');
             xhr.send(fd);
         }, false);
+    },
+    uploadMultipleImgs: function (inputID, divID, uploadingCallback, doneUploadCallback) {
+        document.getElementById(inputID).addEventListener('change', function (e) {
+            uploadingCallback();
+            var file = e.target.files[0];
+            if (!file || !file.type.match(/image.*/))
+                return;
+            var fd = new FormData();
+            fd.append("image", file); // Append the file
+            var xhr = new XMLHttpRequest(); // Create the XHR (Cross-Domain XHR FTW!!!) Thank you sooooo much imgur.com
+            xhr.open("POST", "https://api.imgur.com/3/image"); // Boooom!
+            xhr.onload = function () {
+                if (xhr.status == 200) {
+                    let img = document.createElement('img');
+                    img.src = JSON.parse(xhr.responseText).data.link;
+                    document.getElementById(divID).appendChild(img);
+                    doneUploadCallback();
+                }
+                else {
+                    window.alert('ImgurXHR error: Error in uploading... Please try again');
+                    imgLink.error = "Error Uploading in ImgUr";
+                }
+            }
+            xhr.setRequestHeader('Authorization', 'Client-ID 33f63d5902f27e5');
+            xhr.send(fd);
+        }, false);
     }
 }
 
