1cdba06174cc529697dd103705546a55c7d4dfd1
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-13 19:13:18 +0800

    Update modalCreator.js

--- modalCreator.js
+++ modalCreator.js
@@ -36,5 +36,21 @@ var ModalCreator = {
                 doc.remove();
             }
         }
+    },
+    popFunction: function (modalID, titleText, bodyText, buttonIcon, buttonText, buttonFunc) {
+        let doc = document.createElement('div');
+        let body = '<!--The Modal--> <div class="modal" id="' + modalID + '"> <!--Modal content--> <div class="ui info message modal-content"> <div class="header">' + titleText + '</div><div class="content"> <small>' + bodyText + '</small><br><br> <a onclick="javascript:' + buttonFunc() + '" class="ui blue image label"><i class="' + buttonIcon + '"></i>' + buttonText + '</a></div> </div> </div>';                                      
+        doc.innerHTML = body;
+        document.querySelector('body').appendChild(doc);
+        let modal = document.getElementById(modalID);
+        let content = modal.querySelector(".modal-content");
+        content.style.width = '30%';
+        modal.style.display = "block";
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+                doc.remove();
+            }
+        }
     }
 }
