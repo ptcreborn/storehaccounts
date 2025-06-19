dfc6da4e4d652a2afd34f053ded40e1d4ee94c3f
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-19 15:51:13 +0800

    Create OfficialDownloadCounter.js

---
+++ OfficialDownloadCounter.js
@@ -0,0 +1,73 @@
+
+
+// This function serves all Download buttons
+// that has been registered to Firebase
+// and Mediafire Links
+// Checking the number of ads to be bypassed
+
+
+(async () => {
+    await initFunctions(['FirebaseModule', 'PTC_Cookies']);
+    let allBtns = document.querySelectorAll('button[disabled]');
+    let filterBtn = Array.from(allBtns);
+    filterBtn = filterBtn.filter(items => {
+        return items.id.substring(0, 4) === "mdm-" ? items : null;
+    });
+
+    for (let i = 0; i < filterBtn.length; i++) {
+        let btn = filterBtn[i];
+        let key = '-' + filterBtn[i].id.split('mdm-')[1];
+        let db = 'https://account-requests-default-rtdb.firebaseio.com/shortenedLinks';
+        const official_db = 'https://ptc-database-default-rtdb.firebaseio.com/mods';
+        let uid = '';
+
+        let data = await FirebaseModule.get(db + '/' + key + '.json');
+        data = JSON.parse(data);
+
+        let title = data.title;
+        let version = data.version;
+        let numads = data.numads;
+
+        btn.onclick = async () => {
+            if (PTC_Cookies.checkIfStorageSupported) {
+                data = await FirebaseModule.get(db + '/' + key + '.json');
+                data = JSON.parse(data);
+
+                // creating id
+                // stores for 20 minutes
+                if (data.hasOwnProperty('numads')) {
+                    PTC_Cookies.storeLocalStorage(btoa(key),
+                        JSON.stringify({
+                            'click': 0,
+                            'numads': data.numads
+                        }));
+                    uid = 'download=' + data.title + '&id=' + btoa(key) + '&ver=' + data.version;
+                } else {
+                    uid = key;
+                }
+
+                await FirebaseModule.patch(db + '/' + key + '.json', JSON.stringify({
+                    'count': parseInt(data.count) + 1
+                }));
+                
+                btn.style.pointerEvents = 'none';
+                btn.style.opacity = '0.8';
+                if (numads == 0)
+                    window.location.href = 'https://storehaccounts.blogspot.com/p/mediafire-link-generator.html?download=' + title + '&id=' + btoa(key) + '&ver=' + version;
+                else
+                    window.location.href = 'https://storehaccounts.blogspot.com/p/link-terminal.html?' + uid;
+            } else {
+                window.alert("Please Enable Cookies in your browser. You can use Incognito mode or Private Mode. If this is a problem please email jasonbourne181997@gmail.com.");
+            }
+
+            // incrementing official download count
+            if (!document.querySelector('[app-signature]')) return;
+            let dl = await FirebaseModule.fetchJSON(`${official_db}/${btoa(document.querySelector('[app-signature]').innerText)}/downloads.json`);
+            dl += 1;
+            await FirebaseModule.patch(`${official_db}/${btoa(document.querySelector('[app-signature]').innerText)}.json`, JSON.stringify({
+                downloads: dl
+            }));
+        }
+        btn.removeAttribute('disabled');
+    }
+})();
