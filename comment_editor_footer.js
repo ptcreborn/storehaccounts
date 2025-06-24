2000feba45d208c3bcba15a099fc4cc82c860597
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-24 18:55:31 +0800

    Update comment_editor_footer.js

--- comment_editor_footer.js
+++ comment_editor_footer.js
@@ -1,6 +1,6 @@
 (async () => {
   // appending Quill script after load
-  if(!document.getElementById('ptc_comment_editor')) return;
+  if (!document.getElementById('ptc_comment_editor')) return;
 
   const postBtn = document.getElementById('postBtn');
   const editor = document.getElementById('ql-comment-editor');
@@ -24,6 +24,7 @@
     script.src = 'https://cdn.jsdelivr.net/npm/quill@2.0.3/dist/quill.js';
     document.querySelector('body').appendChild(script);
 
+    script = document.createElement('script');
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js';
     document.querySelector('body').appendChild(script);
   })();
