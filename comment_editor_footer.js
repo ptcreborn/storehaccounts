424f8dac096d73e7e95ac513eefdd5ef84f95a8f
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-26 15:16:30 +0800

    Update comment_editor_footer.js

--- comment_editor_footer.js
+++ comment_editor_footer.js
@@ -224,7 +224,7 @@
     postBtn.innerHTML = `Comment Posted!!`;
     postBtn.classList.add('green');
     setTimeout(() => {
-      if (!document.querySelector('#comments_container')) return;
+      if (!document.querySelector('#ptc_comment_container')) return;
 
       let div = document.createElement('div');
       let div_html = `<div class="ui ignored warning message" style="min-height: 100px;">
@@ -239,7 +239,7 @@
 <br>
 ${getContent()}`;
       div.innerHTML = div_html;
-      document.querySelector('#comments_container').appendChild(div);
+      document.querySelector('#ptc_comment_container').appendChild(div);
 
 
       postBtn.classList.remove('green');
