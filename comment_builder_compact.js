1f833de37f376f85644f70d52362b62dfdaf3cc9
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-06-27 14:40:57 +0800

    Update comment_builder_compact.js

--- comment_builder_compact.js
+++ comment_builder_compact.js
@@ -79,9 +79,9 @@
         // After comment has been built, lets check if there are replies within it..
         // checking for replies within a comment
         let replies_data = await getRepliesData(items.comments.id);
-        if(!replies_data) return;
         
-        for(const reply of replies_data) {
+        if(replies_data)
+            for(const reply of replies_data) {
             let reply_user = reply.users;
             let reply_content = reply.replies;
             let reply_user_rank = reply_user.ranks;
