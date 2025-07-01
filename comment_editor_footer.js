bb3d2122f423fe3effaa907f0a1efb59eae515e2
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-01 22:30:11 +0800

    Update comment_editor_footer.js

--- comment_editor_footer.js
+++ comment_editor_footer.js
@@ -248,7 +248,7 @@
         date: "now()",
         content: getContent(),
         images: img_json_arr,
-        url: new URL(window.location.href).pathname + '/?comment=' + commentid.replace('ptc-child-comment-', '')
+        url: new URL(window.location.href).pathname + '?comment=' + commentid.replace('ptc-child-comment-', '')
       }).select('id');
 
       if (data.error) {
