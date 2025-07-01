34893be40d277e73788f20af89783674b86ddac6
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-01 18:45:42 +0800

    Update comment_builder_compact.js

--- comment_builder_compact.js
+++ comment_builder_compact.js
@@ -4,47 +4,47 @@
 // June 25 2025....
 
 (async() => {
-    if (window.location.href.includes('/p/') || !document.querySelector('.postBody'))
-        return;
-
-    await initFunctions(['supabase', 'moment']);
-
-    let url = new URL(window.location.href);
-    url = url.pathname;
-    let isViewingComment = false;
-
-    const comments_container = document.querySelector('#ptc_comment_container');
-    const comments_count_container = document.querySelector('#ptc_comment_count');
-    comments_container.classList.add('notification-parent-comments');
-    let queried_comment_elem = '';
-
-    // check if the cookies for all comments are stored
-    if (sessionStorage.getItem(url)) {
-        let cached_comment = JSON.parse(sessionStorage.getItem(url));
-        let ms_expiration = 120000;
-
-        if (new Date().getTime() - (parseInt(cached_comment.date)) < ms_expiration) {
-            comments_container.innerHTML = cached_comment.content;
-            comments_count_container.innerHTML = cached_comment.count;
-
-            queried_comment_elem = extractCommentIDQuery();
-            if (queried_comment_elem)
-                scrollIntoViewport(queried_comment_elem);
+        if (window.location.href.includes('/p/') || !document.querySelector('.postBody'))
             return;
-        }
-    }
 
-    const postid = await getPostID(url);
-    if (!postid) {
-        comments_count_container.innerHTML = `<br/><br/><h4 class="ui inverted horizontal divider header">
+        await initFunctions(['supabase', 'moment']);
+
+        let url = new URL(window.location.href);
+        url = url.pathname;
+        let isViewingComment = false;
+
+        const comments_container = document.querySelector('#ptc_comment_container');
+        const comments_count_container = document.querySelector('#ptc_comment_count');
+        comments_container.classList.add('notification-parent-comments');
+        let queried_comment_elem = '';
+
+        // check if the cookies for all comments are stored
+        if (sessionStorage.getItem(url)) {
+            let cached_comment = JSON.parse(sessionStorage.getItem(url));
+            let ms_expiration = 120000;
+
+            if (new Date().getTime() - (parseInt(cached_comment.date)) < ms_expiration) {
+                comments_container.innerHTML = cached_comment.content;
+                comments_count_container.innerHTML = cached_comment.count;
+
+                queried_comment_elem = extractCommentIDQuery();
+                if (queried_comment_elem)
+                    scrollIntoViewport(queried_comment_elem);
+                return;
+            }
+        }
+
+        const postid = await getPostID(url);
+        if (!postid) {
+            comments_count_container.innerHTML = `<br/><br/><h4 class="ui inverted horizontal divider header">
 			<i class="comments icon"></i>
 			Be the first one to comment!
 			</h4><br/><br/>`;
-        return;
-    }
+            return;
+        }
 
-    let user_comments_data = await getCommentsAndUsersData(postid);
-    comments_count_container.innerHTML = `<br/><br/><h4 class="ui inverted horizontal divider header">
+        let user_comments_data = await getCommentsAndUsersData(postid);
+        comments_count_container.innerHTML = `<br/><br/><h4 class="ui inverted horizontal divider header">
                                 		<i class="comments icon"></i>
                                 		${user_comments_data.length < 2 ? `${user_comments_data.length} comment` : ` ${user_comments_data.length} comments`}</h4><br/><br/>`;                         
                                                                                 
@@ -146,18 +146,12 @@
     async function checkQueryComment() {
         let comment_id = extractCommentIDQuery();
 
-        console.log(`comment_id ${comment_id}`);
-
         if(!comment_id) return;
 
         let data = await getCommentsAndUsersDataviaCommentID(comment_id);
 
-        console.log(`data: ${data}`);
-
         let items = data;
-
-        console.log(`items ${items}`);
-
+        
         let clonedTemplate = qts('comment-container').cloneNode(true).content.children[0];
         clonedTemplate.id = `ptc-child-comment-${items.comments.id}`;
 
@@ -188,6 +182,54 @@
         qt(clonedTemplate, 'thread-rank').querySelector('span').innerText = `Rank ${items.users.rank_id} ${ranks_data.rank_name}`;
 
         comments_container.appendChild(clonedTemplate); 
+
+        let comment_temp_container = clonedTemplate;
+
+        // After comment has been built, lets check if there are replies within it..
+        // checking for replies within a comment
+        let replies_data = await getRepliesData(comment_id);
+        if (replies_data)
+            for (const reply of replies_data) {
+                let reply_user = reply.users;
+                let reply_content = reply.replies;
+                let reply_user_rank = reply_user.ranks;
+
+                let clonedTemplate = qts('comment-container').cloneNode(true).content.children[0];
+                clonedTemplate.id = `ptc-child-reply-${reply_content.id}`;
+                clonedTemplate.className = 'ui warning message';
+
+                let country_name = '';
+
+                if (reply_user.country == "Anonymous") {
+                    country_name = 'Homeless Catter';
+                } else {
+                    country_name = await fetch('https://restcountries.com/v3.1/alpha/' + reply_user.country);
+                    country_name = await country_name.json();
+                    country_name = country_name[0].name.official;
+                }
+
+                // build user data first
+                qt(clonedTemplate, 'thread-user-img').src = reply_user.prof_img;
+                qt(clonedTemplate, 'thread-country').querySelector('img').src = `${reply_user.country == "Anonymous" ? ` https: //static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${reply_user.country}/shiny/64.png`}`;
+                    qt(clonedTemplate, 'thread-country').querySelector('span').innerText = `${country_name}`;
+                qt(clonedTemplate, 'thread-user-name').innerText = reply_user.username;
+
+                // build comments info
+                qt(clonedTemplate, 'thread-comments').innerHTML = reply_content.content;
+                qt(clonedTemplate, 'thread-action').innerText = "commented";
+                qt(clonedTemplate, 'thread-time-ago').innerText = moment(new Date(reply_content.date)).fromNow();
+
+                // build rank info
+                let ranks_data = await getRanksData(reply_user_rank.id);
+                qt(clonedTemplate, 'thread-rank').querySelector('img').src = ranks_data.rank_image;
+                qt(clonedTemplate, 'thread-rank').querySelector('span').innerText = `Rank ${reply_user_rank.id} ${ranks_data.rank_name}`;
+
+                qt(clonedTemplate, 'thread-reply').addEventListener('click', () => {
+                    clonedTemplate.parentNode.querySelector('[thread-reply]').click();
+                });
+
+                comment_temp_container.appendChild(clonedTemplate);
+            }  
 
         scrollIntoViewport(comment_id);
     }
