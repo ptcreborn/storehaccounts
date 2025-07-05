44f4a6c32e108d79be859db989f234bac83c9b1a
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-05 11:03:58 +0800

    Update hillTopAds.js

--- hillTopAds.js
+++ hillTopAds.js
@@ -1,30 +1,22 @@
 (async() => {
-    let html = `<script defer='defer' async='async'>
-(function(eepr){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = eepr || {};
-s.src = "\/\/revolvingemergency.com\/b\/X.V\/sLdQGHlg0SYUWNct\/TeimK9LuHZSUxlpkcPRTZYO0-NSzegQyQN-T-g\/t\/NDjhQH3ZORDZIB1\/OYQW";
-s.async = true;
-s.referrerPolicy = 'no-referrer-when-downgrade';
-l.parentNode.insertBefore(s, l);
-})({})
-</script>
-
-<script defer='defer' async='async'>
-(function(mjhhni){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = mjhhni || {};
-s.src = "\/\/mortifiedcourse.com\/cLD-9A6Rb.2l5ylnSBWjQo9bN\/j\/Q\/3GOMDNI\/0jMJC\/0A2BNvD\/cc4hMkjpQSxA";
-s.async = true;
-s.referrerPolicy = 'no-referrer-when-downgrade';
-l.parentNode.insertBefore(s, l);
-})({})
-</script>`;
-
-    let div = document.getElementById('hillTopAds1');
-    div.innerHTML = html;
+    (function(eepr) {
+        var d = document,
+            s = d.createElement('script'),
+            l = d.scripts[d.scripts.length - 1];
+        s.settings = eepr || {};
+        s.src = "\/\/revolvingemergency.com\/b\/X.V\/sLdQGHlg0SYUWNct\/TeimK9LuHZSUxlpkcPRTZYO0-NSzegQyQN-T-g\/t\/NDjhQH3ZORDZIB1\/OYQW";
+        s.async = true;
+        s.referrerPolicy = 'no-referrer-when-downgrade';
+        l.parentNode.insertBefore(s, l);
+    })({});
+    (function(mjhhni) {
+        var d = document,
+            s = d.createElement('script'),
+            l = d.scripts[d.scripts.length - 1];
+        s.settings = mjhhni || {};
+        s.src = "\/\/mortifiedcourse.com\/cLD-9A6Rb.2l5ylnSBWjQo9bN\/j\/Q\/3GOMDNI\/0jMJC\/0A2BNvD\/cc4hMkjpQSxA";
+        s.async = true;
+        s.referrerPolicy = 'no-referrer-when-downgrade';
+        l.parentNode.insertBefore(s, l);
+    })({});
 })();
