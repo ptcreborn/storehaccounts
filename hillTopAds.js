831fe55a84fb147dc0209709119b07ebfcce393b
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-05 10:56:00 +0800

    Create hillTopAds.js

---
+++ hillTopAds.js
@@ -0,0 +1,32 @@
+(async() => {
+    let html = `<script defer='defer' async='async'>
+(function(eepr){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = eepr || {};
+s.src = "\/\/revolvingemergency.com\/b\/X.V\/sLdQGHlg0SYUWNct\/TeimK9LuHZSUxlpkcPRTZYO0-NSzegQyQN-T-g\/t\/NDjhQH3ZORDZIB1\/OYQW";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+</script>
+
+<script defer='defer' async='async'>
+(function(mjhhni){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = mjhhni || {};
+s.src = "\/\/mortifiedcourse.com\/cLD-9A6Rb.2l5ylnSBWjQo9bN\/j\/Q\/3GOMDNI\/0jMJC\/0A2BNvD\/cc4hMkjpQSxA";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+</script>`;
+
+    let div = document.createElement('div');
+    div.innerHTML = html;
+
+    document.querySelector('body').appendChild(div);
+})();
