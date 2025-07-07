08962847d2d1b8bc562e7d0cd0553bd5ef897e00
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-07 13:29:32 +0800

    Create Oauth2Blogger.js

---
+++ Oauth2Blogger.js
@@ -0,0 +1,23 @@
+// checks whether the user logged in from Blogger Users: Landing Point Page to Home page
+// gets the access token and expiry in seconds...
+
+(() => {
+    let url = window.location.href;
+    url = new URL(url);
+
+    let access_token = '';
+    let expiry_seconds = 0;
+
+    if (!url.hash) return;
+
+    if (!url.hash.includes('#state=pass-through')) return;
+
+    access_token = url.hash.split('&access_token=')[1].split('&token_type=')[0];
+    expiry_seconds = parseInt(url.hash.split('&expires_in=')[1].split('&scope=')[0]);
+
+    localStorage.setItem('userBloggerToken', JSON.stringify({
+        token: access_token,
+        expiry: expiry_seconds
+    }));
+
+})();
