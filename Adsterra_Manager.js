7cff065e2ed5ca04155e238f051dee70a74873f4
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-10 23:10:56 +0800

    Update Adsterra_Manager.js

--- Adsterra_Manager.js
+++ Adsterra_Manager.js
@@ -1,4 +1,4 @@
-(() => { // ADSTERRA MANAGER
+(() => {
     let adsterra_ads = [{
         src: `//pl26726594.profitableratecpm.com/d43c359fe4d2caa5b747df7256cd242b/invoke.js`,
         div: '<div id="container-d43c359fe4d2caa5b747df7256cd242b"></div>',
@@ -7,31 +7,7 @@
         src: `//pl26726594.profitableratecpm.com/d43c359fe4d2caa5b747df7256cd242b/invoke.js`,
         div: '<div id="container-d43c359fe4d2caa5b747df7256cd242b"></div>',
         divid: `adsterra_mid`
-    }, {
-        src: `//www.highperformanceformat.com/cc3b17b3d88c95500f8a72553d34420b/invoke.js`,
-        div: `<script type="text/javascript">
-	atOptions = {
-		'key' : 'cc3b17b3d88c95500f8a72553d34420b',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-<\/script>`,
-        divid: `adsterra_side_box`
-    }, {
-        src: `//www.highperformanceformat.com/e64a9928ce8120f24c524dc9289bfc65/invoke.js`,
-        div: `<script type="text/javascript">
-	atOptions = {
-		'key' : 'e64a9928ce8120f24c524dc9289bfc65',
-		'format' : 'iframe',
-		'height' : 600,
-		'width' : 160,
-		'params' : {}
-	};
-<\/script>`,
-        divid: `adsterra_leaderboard`
-    }];
+    };
 
     adsterra_ads.forEach(items => {
         if (!document.getElementById(items.divid)) return;
@@ -43,9 +19,6 @@
         script.async = 'async';
         script.src = items.src;
 
-        if (items.div.includes('<script>'))
-            document.getElementById(items.divid).appendChild(script);
-        else
-            document.querySelector('body').appendChild(script);
+        document.querySelector('body').appendChild(script);
     });
 })();
