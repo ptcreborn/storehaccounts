d0fd9f6a385c849392c158016d1c2955f67542f5
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-10 22:14:37 +0800

    Update Adsterra_Manager.js

--- Adsterra_Manager.js
+++ Adsterra_Manager.js
@@ -1,54 +1,25 @@
+(() => {
+    let adsterra_ads = [{
+        src: `//pl26726594.profitableratecpm.com/d43c359fe4d2caa5b747df7256cd242b/invoke.js`,
+        div: '<div id="container-d43c359fe4d2caa5b747df7256cd242b"></div>',
+        divid: `adsterra_top`
+    }, {
+        src: `//pl26726594.profitableratecpm.com/d43c359fe4d2caa5b747df7256cd242b/invoke.js`,
+        div: '<div id="container-d43c359fe4d2caa5b747df7256cd242b"></div>',
+        divid: `adsterra_mid`
+    }];
 
+    adsterra_ads.forEach(items => {
+        if(!document.getElementById(items.divid)) return;
+        
+        document.getElementById(items.divid).innerHTML = `${items.div}`;
+        document.getElementById(items.divid).classList.remove('loading');
 
-// this js file will manage all adsterra ads
+        let script = document.createElement('script');
+        script.async = 'async';
+        script.setAttribute('data-cfasync', 'false');
+        script.src = items.src;
 
-
-// ADSTERRA Lazy ADS
-
-(async function () {
-  // PLEASE ADD to array only BANNER!
-    let adsProperty = [[
-            //ADS #1
-            "adsterra_top",
-            "atOptions = { 'key' : 'dbb081bd229885ddc0aa9d909e761042', 'format' : 'iframe', 'height' : 90, 'width' : 720, 'params' : {} };",
-            "//www.highperformanceformat.com/dbb081bd229885ddc0aa9d909e761042/invoke.js"
-        ],
-
-        // ADS#2
-        [
-            "adsterra_mid",
-            "atOptions = { 'key' : 'cc3b17b3d88c95500f8a72553d34420b', 'format' : 'iframe', 'height' : 90, 'width' : 720, 'params' : {} };",
-            "//www.highperformanceformat.com/cc3b17b3d88c95500f8a72553d34420b/invoke.js"
-        ]
-    ];
-
-    // ADDING BANNERS
-    adsProperty.forEach(items => {
-        if (document.getElementById(items[0])) {
-            let tempo_div = document.getElementById(items[0]);
-            let scriptA = document.createElement('script');
-            let scriptB = document.createElement('script');
-
-            scriptA.innerHTML = items[1];
-            scriptB.src = items[2];
-
-            tempo_div.appendChild(scriptA);
-            tempo_div.appendChild(scriptB);
-        }
+        document.querySelector('body').appendChild(script);
     });
-
-    // Native Banner Only! FIXED
-    let scriptt = document.createElement('script');
-    let divv = document.createElement('div');
-
-    scriptt.setAttribute('async', 'async');
-    scriptt.setAttribute('data-cfasync', 'false');
-    scriptt.setAttribute('src', '//pl26726594.profitableratecpm.com/d43c359fe4d2caa5b747df7256cd242b/invoke.js');
-    divv.id = 'container-d43c359fe4d2caa5b747df7256cd242b';
-
-    if (document.querySelector('#adsterra_bottom')) {
-        document.querySelector('#adsterra_bottom').appendChild(divv);
-        document.querySelector('#adsterra_bottom').appendChild(scriptt);
-    }
-
 })();
