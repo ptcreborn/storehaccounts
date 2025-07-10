e8f7ce67def9d9f64c56d0c461d1f78b607cfff0
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-10 23:04:44 +0800

    Update Adsterra_Manager.js

--- Adsterra_Manager.js
+++ Adsterra_Manager.js
@@ -7,6 +7,30 @@
         src: `//pl26726594.profitableratecpm.com/d43c359fe4d2caa5b747df7256cd242b/invoke.js`,
         div: '<div id="container-d43c359fe4d2caa5b747df7256cd242b"></div>',
         divid: `adsterra_mid`
+    }, {
+        src: `//www.highperformanceformat.com/cc3b17b3d88c95500f8a72553d34420b/invoke.js`,
+        div: `<script type="text/javascript">
+	atOptions = {
+		'key' : 'cc3b17b3d88c95500f8a72553d34420b',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+<\/script>`,
+        divid: `adsterra_side_box`
+    }, {
+        src: `//www.highperformanceformat.com/e64a9928ce8120f24c524dc9289bfc65/invoke.js`,
+        div: `<script type="text/javascript">
+	atOptions = {
+		'key' : 'e64a9928ce8120f24c524dc9289bfc65',
+		'format' : 'iframe',
+		'height' : 600,
+		'width' : 160,
+		'params' : {}
+	};
+<\/script>`,
+        divid: `adsterra_leaderboard`
     }];
 
     adsterra_ads.forEach(items => {
