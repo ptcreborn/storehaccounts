91a19bd49715af2371a87274898a0f951d64953b
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-16 13:52:50 +0800

    Update NotificationService.js

--- NotificationService.js
+++ NotificationService.js
@@ -41,10 +41,38 @@
         } else {
             let keys = Object.keys(notifs_data);
             let count_notif = 0;
+            let read_keys = [];
 
             for (key of keys) {
                 let data = notifs_data[key];
 
+                if (!data.read) {
+                    let container = notif_template.content.cloneNode(true).children[0];
+                    container.href = data.href;
+                    if (container.querySelector('[notif-none]')) container.querySelector('[notif-none]').remove();
+                    container.querySelector('[notif-action]').innerText = data.action;
+                    container.querySelector('[notif-prof]').src = data.prof;
+                    container.querySelector('[notif-thumb]').src = data.hasOwnProperty('thumb') ? data.thumb : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/2048px-No_image_available.svg.png';
+
+                    container.querySelector('[notif-title-snippet]').innerHTML = data.title + `<br/><i
+                    style="font-size: 11px; font-weight: 500; color: white; opacity: 0.6;">${data.href}</i>`;
+                    container.querySelector('[notif-user]').innerText = data.user;
+                    container.querySelector('[notif-time]').innerText = moment(new Date(parseInt(key))).fromNow();
+                    notif_container.querySelector('div').appendChild(container);
+
+                    container.addEventListener('click', async() => {
+                        markRead(`https://ptc-database-default-rtdb.firebaseio.com/notifications/${encodedUsername}`, key, data.href);
+                        return false;
+                    });
+                    count_notif++;
+                } else {
+                    read_keys.push(key);
+                    container.querySelector('[notif-unread]').remove();
+                }
+            }
+
+            for (key of read_keys) {
+                let data = notifs_data[key];
                 let container = notif_template.content.cloneNode(true).children[0];
                 container.href = data.href;
                 if (container.querySelector('[notif-none]')) container.querySelector('[notif-none]').remove();
@@ -57,16 +85,7 @@
                 container.querySelector('[notif-user]').innerText = data.user;
                 container.querySelector('[notif-time]').innerText = moment(new Date(parseInt(key))).fromNow();
                 notif_container.querySelector('div').appendChild(container);
-
-                if (!data.read) {
-                    container.addEventListener('click', async() => {                        
-                        markRead(`https://ptc-database-default-rtdb.firebaseio.com/notifications/${encodedUsername}`, key, data.href);
-                        return false;
-                    });
-                    count_notif++;
-                } else {
-                    container.querySelector('[notif-unread]').remove();
-                }
+                container.querySelector('[notif-unread]').remove();
             }
 
             document.querySelector('#notif_count').innerText = count_notif;
