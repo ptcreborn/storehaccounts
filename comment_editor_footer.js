84f98474837f1ac942310d2bab43ee70f06659c5
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-17 23:37:34 +0800

    Update comment_editor_footer.js

--- comment_editor_footer.js
+++ comment_editor_footer.js
@@ -419,6 +419,8 @@
             if (actionText.innerText == "Comment") document.getElementById('ptc_comment_container').appendChild(div);
             else if (actionText.innerText = "Reply") document.getElementById(`${commentid}`).appendChild(div);
 
+            scrollIntoViewportByElement(div);
+
             const comments_container = document.querySelector('#ptc_comment_container');
             const comments_count_container = document.querySelector('#ptc_comment_count');
 
