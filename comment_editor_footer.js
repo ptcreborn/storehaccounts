894f484ce3a71e1dd9bbb49768253bbefdc22d34
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-17 21:03:09 +0800

    Update comment_editor_footer.js

--- comment_editor_footer.js
+++ comment_editor_footer.js
@@ -31,7 +31,7 @@
 
         if (elem.parentNode.id.includes('reply')) {
             replyid = elem.parentNode.id;
-            reply_target = document.getElementById(replyid);
+            reply_target = document.getElementById(elem.id);
 
             while (elem.parentNode.id.includes('reply'))
                 elem = elem.parentNode;
