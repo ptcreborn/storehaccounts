ffe9b3fbafd5ab3ac307b171ed7d37dd887b20c8
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-17 22:24:46 +0800

    Update comment_editor_footer.js

--- comment_editor_footer.js
+++ comment_editor_footer.js
@@ -299,9 +299,11 @@
 
         // STORING REPLY TABLE
         else if (actionText.innerText == "Reply") {
+            console.log(replytargetdummy);
+            console.log(replytargetdummy.outerHTML);
             let data = await supabase.from('replies').insert({
                 date: "now()",
-                content: replyid && !replytargetdummy ? `${replytargetdummy.outerHTML}<br/>${getContent()}` : getContent(),
+                content: replyid ? `${replytargetdummy.outerHTML}<br/>${getContent()}` : getContent(),
                 images: img_json_arr,
                 url: new URL(window.location.href).pathname + '?comment=' + commentid.replace('ptc-child-comment-', '')
             }).select('id');
