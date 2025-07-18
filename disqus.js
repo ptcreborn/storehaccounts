7c8a42e9ac1c994149c0b2645ba7e89019516773
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-07-19 02:31:16 +0800

    Update disqus.js

--- disqus.js
+++ disqus.js
@@ -1,8 +1,7 @@
 window.addEventListener('load', () => {
     var disqus_config = function() {
-        const cannonical_url = new URL(window.location.href);
-        this.page.url = `${cannonical_url.origin}${cannonical_url.pathname}`; // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = `${cannonical_url.origin}${cannonical_url.pathname}`; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        this.page.url = document.querySelector("link[rel='canonical']").href; // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = document.querySelector("link[rel='canonical']").href; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
 
     (function() { // DON'T EDIT BELOW THIS LINE
