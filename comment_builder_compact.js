6a8ee8d00316e5957134da7b17a48550b904f4d5
ptcreborn <144877632+ptcreborn@users.noreply.github.com>
2025-08-07 17:42:59 +0800

    Update comment_builder_compact.js

--- comment_builder_compact.js
+++ comment_builder_compact.js
@@ -114,7 +114,7 @@
 
                 // build user data first
                 qt(clonedTemplate, 'thread-user-img').src = reply_user.prof_img;
-                qt(clonedTemplate, 'thread-country').querySelector('img').src = `${reply_user.country == "Anonymous" ? ` https: //static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${reply_user.country}/shiny/64.png`}`;
+                qt(clonedTemplate, 'thread-country').querySelector('img').src = `${reply_user.country == "Anonymous" ? ` https://static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${reply_user.country}/shiny/64.png`}`;
                     qt(clonedTemplate, 'thread-country').querySelector('span').innerText = `${country_name}`;
                 qt(clonedTemplate, 'thread-user-name').innerText = reply_user.username;
 
@@ -169,7 +169,7 @@
 
         // build user data first
         qt(clonedTemplate, 'thread-user-img').src = items.users.prof_img;
-        qt(clonedTemplate, 'thread-country').querySelector('img').src = `${items.users.country == "Anonymous" ? ` https: //static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${items.users.country}/shiny/64.png`}`;
+        qt(clonedTemplate, 'thread-country').querySelector('img').src = `${items.users.country == "Anonymous" ? ` https://static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${items.users.country}/shiny/64.png`}`;
         qt(clonedTemplate, 'thread-country').querySelector('span').innerText = `${country_name}`;
         qt(clonedTemplate, 'thread-user-name').innerText = items.users.username;
 
@@ -211,7 +211,7 @@
 
                 // build user data first
                 qt(clonedTemplate, 'thread-user-img').src = reply_user.prof_img;
-                qt(clonedTemplate, 'thread-country').querySelector('img').src = `${reply_user.country == "Anonymous" ? ` https: //static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${reply_user.country}/shiny/64.png`}`;
+                qt(clonedTemplate, 'thread-country').querySelector('img').src = `${reply_user.country == "Anonymous" ? ` https://static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${reply_user.country}/shiny/64.png`}`;
                     qt(clonedTemplate, 'thread-country').querySelector('span').innerText = `${country_name}`;
                 qt(clonedTemplate, 'thread-user-name').innerText = reply_user.username;
 
@@ -255,7 +255,7 @@
 
                 // build user data first
                 qt(clonedTemplate, 'thread-user-img').src = reply_user.prof_img;
-                qt(clonedTemplate, 'thread-country').querySelector('img').src = `${reply_user.country == "Anonymous" ? ` https: //static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${reply_user.country}/shiny/64.png`}`;
+                qt(clonedTemplate, 'thread-country').querySelector('img').src = `${reply_user.country == "Anonymous" ? ` https://static.wikia.nocookie.net/361735c0-7535-4dfe-b5d7-6f1683b4550b/scale-to-width/755` : `https://flagsapi.com/${reply_user.country}/shiny/64.png`}`;
                     qt(clonedTemplate, 'thread-country').querySelector('span').innerText = `${country_name}`;
                 qt(clonedTemplate, 'thread-user-name').innerText = reply_user.username;
 
